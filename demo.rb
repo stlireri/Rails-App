c44c9c075bc7d696348f3b4099f107fd191955e8
stella <stellaireri@gmiail.com>
2017-09-13 16:53:27 +0300

    added some files

---
+++ demo.rb
@@ -0,0 +1,39 @@
+x =16
+if x==10
+	puts "x is = 10"
+else
+	puts "x is somthing else"
+end
+
+
+=begin
+	
+end
+#for loop
+for i in 0..6
+	puts i
+end
+
+for i in 0...6
+	puts
+end
+
+
+#while loop
+i=1
+limit = 5
+while i <= limit do 
+	puts "iteration: #{i}"
+	i +=1
+	
+end
+=end
+
+#until loop
+i = 5
+until i<1 do
+	puts "iteration: #{i}"
+	i -= 1
+	
+end
+puts "iteration is over!!"
